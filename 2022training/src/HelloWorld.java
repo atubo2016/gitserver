763bad4ec45798a120d5d459fb9effa2a9f28b1e
test2 <test2@huron.com>
2022-06-06 17:24:53 +0800

    推送远程仓库

--- 2022training/src/HelloWorld.java
+++ 2022training/src/HelloWorld.java
@@ -3,12 +3,8 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello !!!");
-<<<<<<< HEAD
-		System.out.println("Hello second !!!");
-=======
-		System.out.println("Hello first !!!");
->>>>>>> f6c439db88eb6a98292eac48255f3494946d1418
+		System.out.println("Hello 1 !!!");
+		System.out.println("Hello 2 !!!");
 	}
 
 }
