a368d1f102e9a0a263d1d447fe74584e1ee39ece
test1 <test1@huron.com>
2022-06-06 16:58:35 +0800

    no comment

--- 2022training/src/HelloWorld.java
+++ 2022training/src/HelloWorld.java
@@ -4,8 +4,6 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello !!!");
-		System.out.println("commit 2");
-		System.out.println("commit 3-2");
 	}
 
 }
