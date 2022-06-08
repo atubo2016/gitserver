866366ecf4f5717ecb3593ff19c07efde166b57f
test1 <test1@hyron.com>
2022-06-08 17:46:12 +0800

    删除了多余的代码

--- 2022training/src/HelloWorld.java
+++ 2022training/src/HelloWorld.java
@@ -4,14 +4,6 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello 1 !!!");
-        System.out.println("Hello 1-1 !!!");
-        System.out.println("Hello 1-2 !!!");
-		System.out.println("test22222 added");
-		System.out.println("test11111 added");
-		System.out.println("Hello 2 !!!");
-		System.out.println("Hello 2-1 !!!");
-		System.out.println("Hello 2-2 !!!");
-		System.out.println("Hello 2-3 !!!");
 	}
 
 }
