d8885532e72b62a950baa287c95d603019f34411
test1 <test1@hyron.com>
2022-06-09 10:12:58 +0800

    1-3 提交本地仓库

--- 2022training/src/HelloWorld.java
+++ 2022training/src/HelloWorld.java
@@ -7,6 +7,7 @@ public class HelloWorld {
 		System.out.println("Hello 1 !!!");
 		System.out.println("Hello 1-1 !!!");
 		System.out.println("Hello 1-2 !!!");
+		System.out.println("Hello 1-3 !!!");
 	}
 
 }
