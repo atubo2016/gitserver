f9305c22145a65302614d0922c08003f899d6fd5
test1 <test1@hyron.com>
2022-06-10 14:27:35 +0800

    禁止文件覆盖

--- 2022training/src/HelloWorld.java
+++ 2022training/src/HelloWorld.java
@@ -4,13 +4,6 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello 2 !!!");
-		System.out.println("Hello 2-1 !!!");
-		System.out.println("Hello 1-0 !!!");
-		System.out.println("Hello 2-2 !!!");
-		System.out.println("Hello 1 !!!");
-		System.out.println("Hello 1-1 !!!");
-		System.out.println("Hello 1-2 !!!");
-		System.out.println("Hello 1-3 !!!");
 	}
 
 }
